d2b70a3d5c5eb3a1fc3fa7b5cbeb6e58086927ea
Kleber Vales <klebervales.dev@gmail.com>
2025-08-14 11:39:08 -0300

    Setup initial Spring Boot and JUnit 5

---
+++ src/main/java/com/example/springintegrationtestdemo/SpringIntegrationTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintegrationtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationTestDemoApplication.class, args);
+    }
+
+}
